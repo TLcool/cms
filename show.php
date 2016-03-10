864eb509f2de1580cc2df9abb721b9c24aa924e8
ted <16161576@qq.com>
2016-03-10 23:08:23 +0800

    宝宝时间计算
    宝宝时间计算

---
+++ show.php
@@ -0,0 +1,16 @@
+<?php
+
+		$firstDate=strtotime('2015-10-12');
+		$now=time();
+		$day=($now-$firstDate)/(60*60*24) ;
+		$week=$day/7;
+		
+		echo "您的宝宝于2015年10月12日开始生长<br/>";
+		echo "今天是".date('Y-m-d')."日<br/>";
+		echo "宝宝已经".floor($day)."天了"."<br/>".floor($week)."周";
+
+?>
+
+
+
+
